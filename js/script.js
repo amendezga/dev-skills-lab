39e89583ff839b0a4dcba12192dc3ce3e968f9ff
Alejandro <amendez1361@bths.edu>
2022-12-14 22:41:31 -0500

    fixed formatting, final push

--- js/script.js
+++ js/script.js
@@ -4,24 +4,26 @@ const addX = document.createElement("div")
 
 function removeSkill() {
     $('li').click((e) => {
-       $(e.target).remove();
-   });
- }
+        $(e.target).remove();
+    });
+}
 
 function addSkill(e) {
     if ($('#skills').val() === '') {
-      e.preventDefault();
-      return;
+        e.preventDefault();
+        return;
     }
-  
+
     let skillName = $('#skills').val();
     let listEl = $('.skills-list');
-    let newLiEl = $('<li>', { text: skillName });
+    let newLiEl = $('<li>', {
+        text: skillName
+    });
     listEl.append(newLiEl);
     $('#skills').val('');
     removeSkill();
     e.preventDefault();
-  }
+}
 
 $("form").on("submit", addSkill);
 
